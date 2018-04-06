e71d4b2e163ffbfa9d524ffff434e862f561a9b6
Jun <s.h.h.n.j.k@gmail.com>
2018-04-06 15:43:35 +0100

    Added custom parameter
    Use it with your own creativity :)

--- dysw.js
+++ dysw.js
@@ -1,6 +1,6 @@
 onfetch = event => {
   var params = self.location.href.split("?")[1].split("&");
-  var cors, match, url;
+  var cors, match, url, custom;
   params.forEach(param => {
     if(param.startsWith("cors")){
       cors = param.split("=")[1];
@@ -8,10 +8,23 @@ onfetch = event => {
       match = param.split("=")[1];
     }else if(param.startsWith("url")){
       url = param.split("=")[1];
+    }else if(param.startsWith("custom")){
+      custom = param.split("=")[1];
     }
   });
 
   if(event.request.url.endsWith(match)){
-    event.respondWith(fetch(url, {mode: cors}));
+    console.log("Match: " + event.request.url);
+    if(custom){
+      var split = event.request.url.split("/");
+      for(var i=3; i < split.length; i++){
+        custom += "/" + split[i];
+      }
+      custom = custom.split(match)[0];
+      event.respondWith(fetch(custom, {mode: cors}));
+    }else{
+      event.respondWith(fetch(url, {mode: cors}));
+    }
+
   }
 }
